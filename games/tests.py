cf4e886c4d7057ecb0c2101e3c802fc4a0339725
Adrian Alexander Eriksen <adrian@aeriksen.com>
2013-08-25 21:22:15 +0200

    Added Game-model

--- games/tests.py
+++ games/tests.py
@@ -1,16 +1,23 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+from .models import Game
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class GameModelTest(TestCase):
+    """
+    Creates two games and checks if both is saved, games should be retrieved in ASC order
+    """
+    def test_save_and_retrieving_games(self):
+        first_game = Game()
+        first_game.name = "First game"
+        first_game.save()
+
+        second_game = Game()
+        second_game.name = "Second game"
+        second_game.save()
+
+        saved_games = Game.objects.all()
+
+        self.assertEqual(saved_games.count(), 2)
+        self.assertEqual(saved_games[0].name, 'First game')
+        self.assertEqual(saved_games[1].name, 'Second game')
