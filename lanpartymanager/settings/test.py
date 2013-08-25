d090259312963b7042e8ca0b2a8c92182a6ebfce
Adrian Alexander Eriksen <adrian@aeriksen.com>
2013-08-25 20:56:07 +0200

    Added test environment

---
+++ lanpartymanager/settings/test.py
@@ -0,0 +1,20 @@
+from .core import *
+
+
+# Debug settings
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Admin and mangager settings
+ADMINS = (
+    ('Adrian Alexander Eriksen', 'adrianeriksen@users.noreply.github.com'),
+)
+
+MANAGERS = ADMINS
+
+# Database settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
